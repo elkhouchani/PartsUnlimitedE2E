0dbe73ca07771e518df25a03ca30418e909be164
Siham EL KHOUCHANI <sihamelkhouchani@gmail.com>
2023-03-21 11:49:31 +0000

    My new commit

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        //This is a comment
+        //This is a comment a new comment
         [Key]
         public int CartItemId { get; set; }
 
